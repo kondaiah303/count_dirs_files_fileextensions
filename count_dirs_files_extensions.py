4ec6007f1781e722baa968bf782291fdf4e9db07
Entropik <entropik@192.168.100.104>
2023-02-12 22:30:22 +0530

    folder name changed and code modified

--- count_dirs_files_extensions.py
+++ count_dirs_files_extensions.py
@@ -1,5 +1,4 @@
 import os
-import json
 path = r'/Users/entropik/Documents/code/converz-collab'
 
 
@@ -7,27 +6,21 @@ def count_dirs_files_and_file_extensions(dir_path):
     dirs = 0
     files = 0
     file_extensions = {}
-    count = 0
+    no_extension_file_count = 0
     for dir_root, dir_names, file_names in os.walk(dir_path):
         dirs += len(dir_names)
         for filename in file_names:
             files += 1
             extension = os.path.splitext(filename)[1]
             if extension == '':
-                count += 1
+                no_extension_file_count += 1
                 continue
             if extension in file_extensions:
                 file_extensions[extension] += 1
             else:
                 file_extensions[extension] = 1
     sorted_file_extensions = dict(sorted(file_extensions.items(), key=lambda value: value[1]))
-    body = {
-        'folders_count': dirs,
-        'files_count': files,
-        'extensions_count': sorted_file_extensions,
-        'non_extensions_file_count': count
-    }
-    return json.dumps(body, indent=3)
+    return dirs, files, sorted_file_extensions, no_extension_file_count
 
 
 print(count_dirs_files_and_file_extensions(path))
